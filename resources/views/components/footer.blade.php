b698adf2f9fbeac80e2f385a6ecac7f433736dee
FRANCK KASONGO <109921642+Franckkasongo15@users.noreply.github.com>
2024-06-17 12:24:31 +0200

    fixation de la barre de tache et le copyright

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-7 bg-gray-900">
 
     <div>
-        <h5 class="text-center text-gray-300 text-sm">&copy; 2023 KAZI. Copyright all Rights reserved.</h5>
+        <h5 class="text-center text-gray-300 text-sm">&copy; 2024 KAZI. Copyright all Rights reserved.</h5>
     </div>
 </footer>
